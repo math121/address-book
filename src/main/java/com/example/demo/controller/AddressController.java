bff233617957ac1769cfc0baeec2c6e6b56e1e2d
Mathangi Pushparajah <mathangi23@outlook.com>
2024-04-23 13:36:38 +0200

    Fixed CORS error by adding @CrossOrigin

--- src/main/java/com/example/demo/controller/AddressController.java
+++ src/main/java/com/example/demo/controller/AddressController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+@CrossOrigin
 @RestController
 @RequestMapping("/address-book")
 //TODO Fix validation here after adding it to Address class
