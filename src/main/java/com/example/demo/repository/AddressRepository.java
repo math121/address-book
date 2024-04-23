fbea1975aaf46beee2e7ff1444dac2f7402b350c
Mathangi Pushparajah <mathangi23@outlook.com>
2024-04-23 22:32:22 +0200

    Edited email data for the address objects

--- src/main/java/com/example/demo/repository/AddressRepository.java
+++ src/main/java/com/example/demo/repository/AddressRepository.java
@@ -18,11 +18,11 @@ public class AddressRepository {
         addressBook.add(new Address(111,"Mary", "Clarinton", "Bocosto",
                 "Prinsens gate 12", 23452345, "mary.clarinton@yahoo.com", 987698761, 234523));
         addressBook.add(new Address(112,"John", "Karken", "Kalis",
-                "Dronning gate 12", 45678123, "mary.clarinton@yahoo.com", 34587567, 234412));
+                "Dronning gate 12", 45678123, "john.karken@gmail.com", 34587567, 234412));
         addressBook.add(new Address(113,"Stella", "Tare", "Smart",
-                "Jernbanetorget", 98712234, "mary.clarinton@yahoo.com", 9889912, 7456234));
+                "Jernbanetorget", 98712234, "stella.tare@outlook.com", 9889912, 7456234));
         addressBook.add(new Address(114, "Anya", "Floyer", "HP",
-                "Olsens gate 6", 98712334, "mary.clarinton@yahoo.com", 12399002, 345654));
+                "Olsens gate 6", 98712334, "anya.floyer@hotmail.com", 12399002, 345654));
     }
 
     public List<Address> getAddressBook() {
